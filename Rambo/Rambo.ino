628df4d70c7f750b279fd834525af090981ed009
lvanhulle <lvanhulle@wisc.edu>
2016-10-03 18:58:06 -0500

    Starting final sketch.

---
+++ Rambo/Rambo.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <math.h>
+
+// E0
+int E0_enable = 26; // low == enabled
+int E0_step = 34;
+int E0_dir = 43;
+int E0_MS1 = 65;
+int E0_MS2 = 66;
+int E0_digipot_channel = 0;
+int E0_steps_per_mm = 
+
+// Digipot
+int slave_select_pin = 38;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
